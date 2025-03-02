8ddb8e93813e6634c80ced2a770d8d4a5bf3baad
Huỳnh Minh Trí <mtrigenius@gmail.com>
2025-03-03 01:00:46 +0700

    Fix server crash

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
