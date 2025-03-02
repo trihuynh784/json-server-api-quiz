89fb9d52f4301e3e3d61f1c3e59cc653c1ad1eee
Huỳnh Minh Trí <mtrigenius@gmail.com>
2025-03-03 01:06:19 +0700

    Update db

--- server.js
+++ server.js
@@ -6,7 +6,5 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// Xuất module thay vì chạy trực tiếp
+module.exports = server;
